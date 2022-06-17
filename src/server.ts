bcf09b21cf7ef5bef0f554800cbea305bd3261f0
Sofiyullah abdullah <habsoff@gmail.com>
2022-06-17 14:47:06 +0100

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
@@ -18,7 +18,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // IT SHOULD
   //
   //    1. validate the image_url query
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     let { image_url } = req.query;
 
     if (!image_url) {
